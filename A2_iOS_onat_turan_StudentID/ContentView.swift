1501d75278ea0786f0ca390aafc3636d7334a30d
OnatTuran <onatturann@gmail.com>
2025-03-28 16:42:09 -0400

    uptaded

--- A2_iOS_onat_turan_StudentID/ContentView.swift
+++ A2_iOS_onat_turan_StudentID/ContentView.swift
@@ -44,3 +44,24 @@ struct ContentView: View {
                 }
                 .padding()
 
+                List(products) { product in
+                    VStack(alignment: .leading) {
+                        Text(product.name).font(.headline)
+                        Text(product.desc).font(.subheadline)
+                    }
+                }
+            }
+            .navigationTitle("Product Management")
+        }
+    }
+
+    func addProduct() {
+        guard let price = Double(productPrice) else { return }
+        let newProduct = Product(productID: Int64(products.count + 1), name: productName, desc: productDesc, price: price, provider: productProvider)
+        modelContext.insert(newProduct)
+    }
+
+    func searchProduct() {
+        _products = Query(filter: #Predicate { $0.name.contains(searchKeyword) || $0.desc.contains(searchKeyword) })
+    }
+}
