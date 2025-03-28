021a3da752de748bbe5ae6c72539132b3493f978
OnatTuran <onatturann@gmail.com>
2025-03-28 16:43:33 -0400

    uptaded

--- A2_iOS_onat_turan_StudentID/ContentView.swift
+++ A2_iOS_onat_turan_StudentID/ContentView.swift
@@ -30,7 +30,19 @@ struct ContentView: View {
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
                 
+                Button("Add Product") {
+                    addProduct()
+                }
+                .padding()
+                
+                TextField("Search Product", text: $searchKeyword)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
 
+                Button("Search") {
+                    searchProduct()
+                }
+                .padding()
 
                 List(products) { product in
                     VStack(alignment: .leading) {
