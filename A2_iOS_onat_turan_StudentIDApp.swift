d5d029bad17776332311f316c64cdc7555b10170
OnatTuran <onatturann@gmail.com>
2025-03-28 16:42:41 -0400

    uptaded

--- A2_iOS_onat_turan_StudentIDApp.swift
+++ A2_iOS_onat_turan_StudentIDApp.swift
@@ -1,1 +1,13 @@
+import SwiftUI
+import SwiftData
+
+@main
+struct A2_iOS_onat_turan_StudentIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .modelContainer(for: Product.self)
+        }
+    }
+}
 
