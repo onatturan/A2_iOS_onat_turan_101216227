cdba09c23cf6cef1577bb32533b8f84ed487be7e
OnatTuran <onatturann@gmail.com>
2025-03-28 16:40:38 -0400

    uptaded

--- Product.swift
+++ Product.swift
@@ -8,6 +8,11 @@ class Product {
     var price: Double
     var provider: String
 
-
+    init(productID: Int64, name: String, desc: String, price: Double, provider: String) {
+        self.productID = productID
+        self.name = name
+        self.desc = desc
+        self.price = price
+        self.provider = provider
     }
 }
